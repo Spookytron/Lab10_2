fa7178b79e1acea5e280a8d42454ff1c4f22a0fb
Cameron Power <cameronpower@quickenloans.com>
2021-09-14 16:24:52 -0400

    lab 10.2

---
+++ Lab10_2/Controllers/CoffeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+using Lab10_2.Models;
+
+
+namespace Lab10_2.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop2;Uid=root;Password=abc123");
+            List<Product> prod = db.GetAll<Product>().ToList();
+            return View(prod);
+        }
+        public IActionResult Detail(int pr)
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop2;Uid=root;Password=abc123");
+            Product prod = db.Get<Product>(pr);
+            return View(prod);
+        }
+    }
+}
---
+++ Lab10_2/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
+
+namespace Lab10_2.Models
+{
+    [Table("product")]
+    public class Product
+    { 
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public string category { get; set; }
+        public override string ToString()
+        {
+            return $"{id} {name} {description} {price} {category}";
+        }
+    }
+}
